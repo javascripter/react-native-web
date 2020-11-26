e61f3deaed34b2ad35943bc1f7ab66a548b6cce2
Tsubasa Sakai <javascripter@tsukkun.net>
2020-11-26 15:00:31 +0900

    prevent crash caused by calling prefersReducedMotionMedia.addEventListener, which does not exist on Safari 13 or below

--- packages/react-native-web/src/exports/AccessibilityInfo/index.js
+++ packages/react-native-web/src/exports/AccessibilityInfo/index.js
@@ -61,7 +61,13 @@ const AccessibilityInfo = {
       const listener = event => {
         handler(event.matches);
       };
-      prefersReducedMotionMedia.addEventListener('change', listener);
+
+      //Prior to Safari 14, addListener() and removeListener() must be used fors MediaQueryList
+      if (prefersReducedMotionMedia.addEventListener) {
+        prefersReducedMotionMedia.addEventListener('change', listener);
+      } else {
+        prefersReducedMotionMedia.addListener('change', listener);
+      }
       handlers[handler] = listener;
     }
 
@@ -90,7 +96,11 @@ const AccessibilityInfo = {
         return;
       }
 
-      prefersReducedMotionMedia.removeEventListener('change', listener);
+      if (prefersReducedMotionMedia.addEventListener) {
+        prefersReducedMotionMedia.removeEventListener('change', listener);
+      } else {
+        prefersReducedMotionMedia.removeListener('change', listener);
+      }
     }
 
     return;
